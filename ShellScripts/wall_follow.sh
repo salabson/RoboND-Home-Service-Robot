2503cc5ff0cbc8c71bc5068e1cb4dcb616380ceb
Salabson <salabson4@yahoo.com>
2018-11-06 13:44:25 +0000

    Modify wall_follower script file to referenc my customized world

--- ShellScripts/wall_follow.sh
+++ ShellScripts/wall_follow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
-sleep 20
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_serve.world" &
+sleep 10
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 10
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
