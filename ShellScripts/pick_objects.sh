1f47c0ad1977b3d114fd62011e90f44bbf386700
Salabson <salabson4@yahoo.co.uk>
2018-11-07 13:24:43 +0000

    Modify pick_objects script file to include map path

--- ShellScripts/pick_objects.sh
+++ ShellScripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service.world" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find wall_follower)/map/home_service_map" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
