1254c68542d55b101d804aeedbff757cbc5fdb87
Salabson <salabson4@yahoo.co.uk>
2018-11-08 14:00:07 +0000

    Remove robot pose from wall follower and test navigation script files

--- ShellScripts/wall_follow.sh
+++ ShellScripts/wall_follow.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "export ROBOT_INITIAL_POSE='-x -4.0 -y 4.0 -Y 1.57079633' && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service.world" &
 sleep 10
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 10
