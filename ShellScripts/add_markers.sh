0dbd9f1ec8e6bce0ab236d36bb37d56341ad29cc
Salabson <salabson4@yahoo.co.uk>
2018-11-21 10:16:57 +0000

    Add add_markers script file

---
+++ ShellScripts/add_markers.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm -e "export ROBOT_INITIAL_POSE='-x -6.0 -y 1.5 -Y 1.57079633' && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service.world" &
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/wall_follower/map/home_service_map.yaml" &
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e "rosrun add_markers add_markers" 
